ae20314a30e00c3c532f938f61f1924a749245be
athena272 <guilhermera272@gmail.com>
2022-09-14 05:31:51 -0300

    Update background_image.dart

--- lib/widgets/background_image.dart
+++ lib/widgets/background_image.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class BackgroundImage extends StatelessWidget {
-  const BackgroundImage({
+  BackgroundImage({
     Key? key,
   }) : super(key: key);
 
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1542396601-dca920ea2807?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=751&q=80',
+    'https://images.unsplash.com/photo-1536152470836-b943b246224c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=438&q=80',
+    'https://images.unsplash.com/photo-1548567117-02328f050eaa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ShaderMask(
-      shaderCallback: (bounds) => LinearGradient(
-              colors: [Colors.black, Colors.black12],
-              begin: Alignment.bottomCenter,
-              end: Alignment.center)
-          .createShader(bounds),
-      blendMode: BlendMode.darken,
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: const AssetImage('assets/images/login_bg.png'),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken)),
-        ),
+    final double height = MediaQuery.of(context).size.height;
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: height,
+        viewportFraction: 1.0,
+        enlargeCenterPage: false,
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 5),
       ),
+      items: imgList
+          .map((item) => Container(
+                child: Center(
+                    child: Image.network(
+                  item,
+                  fit: BoxFit.cover,
+                  height: height,
+                )),
+              ))
+          .toList(),
     );
   }
 }
