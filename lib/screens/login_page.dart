0c4a19aaea582dd926e2459ba27d285c22c4b02b
athena272 <guilhermera272@gmail.com>
2022-09-14 04:00:20 -0300

    Update login_page.dart

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:login_app/palatte.dart';
+import 'package:login_screen/palatte.dart';
 import '../widgets/background_image.dart';
 
 class LoginPage extends StatefulWidget {
