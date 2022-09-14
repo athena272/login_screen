99dcb6074bc4ed0eeb8fcb549fe8ed3bc09e6ebd
athena272 <guilhermera272@gmail.com>
2022-09-14 04:00:16 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:login_app/screens/login_page.dart';
+import 'package:login_screen/screens/login_page.dart';
 
 void main() {
   runApp(const MyApp());
