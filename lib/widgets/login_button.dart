9e01befd2a2d50ae3704b7183d58b47908bd7ecf
athena272 <guilhermera272@gmail.com>
2022-09-14 06:40:21 -0300

    Update login_button.dart

--- lib/widgets/login_button.dart
+++ lib/widgets/login_button.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:login_screen/palatte.dart';
+import 'package:login_screen/screens/home_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class LoginButton extends StatelessWidget {
   const LoginButton({
-    Key? key, required this.buttonText,
+    Key? key,
+    required this.buttonText,
   }) : super(key: key);
 
   final String buttonText;
@@ -13,10 +19,19 @@ class LoginButton extends StatelessWidget {
     return Container(
       width: double.infinity,
       decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(16)),
+          color: Colors.blue, borderRadius: BorderRadius.circular(16)),
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () async {
+          var state = await login();
+
+          if (state) {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => HomePage()));
+          } else {
+            
+          }
+
+        },
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 16),
           child: Text(
@@ -26,5 +41,27 @@ class LoginButton extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<bool> login() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    Uri url = Uri.https("jsonplaceholder.typicode.com", "/todos/1");
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> datas = json.decode(response.body);
+      var refAttribute = datas['userId'];
+      keepAttribute(refAttribute.toString());
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void keepAttribute(String refAttribute) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setString("tokenID", refAttribute);
   }
 }
